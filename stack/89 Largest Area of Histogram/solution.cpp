28c15d6b113f9d54f3dcea6521c6dbab695cd9da
Vedant Mahore <145027745+Vedantmahore27@users.noreply.github.com>
2026-03-12 01:06:31 +0530

    Add two approaches for largest rectangle area problem
    Implemented two approaches for calculating the largest rectangle area in a histogram, one with O(n) time and O(1) space, and another with O(n) time and O(n) space.

--- stack/89 Largest Area of Histogram/solution.cpp
+++ stack/89 Largest Area of Histogram/solution.cpp
@@ -1,3 +1,46 @@
+
+//approach 1 best optimal ---> time O(n) , space O(1)
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int ans=0;
+        for(int i=0;i<n;i++){
+           while(!st.empty() && heights[i] < heights[st.top()]){
+               int indx= st.top();
+               st.pop();
+               if(!st.empty()){
+                 ans = max(ans, heights[indx]*(i-st.top()-1));
+               }
+               else{
+                  ans = max(ans, heights[indx]*i);
+               }
+           }
+           st.push(i);
+        }
+
+        while(!st.empty()){
+            int indx= st.top();
+            st.pop();
+
+            if(!st.empty()){
+               ans= max(ans, heights[indx]*(n-st.top()-1));
+            }
+            else{
+                ans= max(ans, heights[indx]*(n));
+            }
+        }
+
+        return ans;
+
+    }
+};
+
+
+// approach 2  ---> time O(n) , space O(n)
+
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
