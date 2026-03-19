d40067eb81051c9d4bedd344e846e80b2807c13c
Vedant Mahore <145027745+Vedantmahore27@users.noreply.github.com>
2026-03-19 17:12:35 +0530

    Create solution.cpp

---
+++ stack/001 validate stack sequences/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int n = s.size();
+         stack<char> st;
+        for(int i =0;i<n;i++){
+            if(s[i]=='*'){
+                st.pop();
+                continue;
+            }
+            st.push(s[i]);
+        }
+        string ans="";
+
+        while(!st.empty()){
+           ans.push_back(st.top());
+           st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
