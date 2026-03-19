c1fc9439a1a45155fc717708be8e1dbc9f61416b
Vedant Mahore <145027745+Vedantmahore27@users.noreply.github.com>
2026-03-19 17:06:04 +0530

    Create solution.cpp

---
+++ stack/013 validate stack sequence/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        int j=0;
+        stack<int> st;
+
+        for(int i =0;i<n;i++){
+          
+            while(!st.empty() && st.top()==popped[j]){
+                st.pop();
+                j++;
+            }
+              st.push(pushed[i]);
+        }
+          while(!st.empty() && st.top()==popped[j]){
+                st.pop();
+                j++;
+            }
+
+        if(st.empty())return true;
+        return false;
+    }
+};
