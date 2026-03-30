68570c9985f48e73d8ae7bfd45d2e53a5303e2c4
Vedant Mahore <145027745+Vedantmahore27@users.noreply.github.com>
2026-03-31 02:29:43 +0530

    Create solution.cpp

---
+++ Binary Search/010 Search in Rotated Array 2/solution.cpp
@@ -0,0 +1,40 @@
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int start=0;
+        int end=n-1;
+
+        while(start<=end){
+            int mid = start +(end-start)/2;
+                  if(nums[mid]==target)return true;
+            if(nums[mid]==nums[end]&& nums[mid]==nums[start]){
+                start++;
+                end--;
+                continue;
+            }
+      
+            else if(nums[start]<=nums[mid]){
+                  if(nums[start] <= target && target < nums[mid]){
+                         end=mid-1;
+                  }
+                  else{
+                     start=mid+1;
+                  }
+            }
+            else if(nums[mid]<=nums[end]){
+               if(nums[mid] < target && target <= nums[end]){
+                        start=mid+1;
+                  }
+                  else{
+                      end=mid-1;
+                  }
+            }
+        }
+
+        return false;
+    }
+};
